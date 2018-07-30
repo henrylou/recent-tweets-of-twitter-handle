dfcf085a53153af06d5e98858f988679314a5791
Hengyu Lou <hylou34@gmail.com>
2018-07-30 16:27:46 -0700

    Implement search feature

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,9 @@ class PagesController < ApplicationController
 
 	def search
     if !params[:user].nil?
-      @username = params[:user]["name"]
+      @username = params[:user][:name]
+    else
+    	@username = 'COZZiLou'
     end
   end
 end
